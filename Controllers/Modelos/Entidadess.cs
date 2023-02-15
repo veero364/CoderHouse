bc42de1dd902800d40eab2a1cc736279729fff0b
veero364 <veero364@users.noreply.github.com>
2023-02-14 21:32:54 -0300

    test

---
+++ Controllers/Modelos/Entidadess.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Entrega
+{
+
+    #region ClassUser
+    public class Usuarios
+    {
+        //Atributos
+
+        private long _Id;
+        private string _Nombre;
+        private string _Apellido;
+        private string _NombreUsuario;
+        private string _Pasword;
+        private string _Mail;
+
+        //Propiedades (se hacen validaciones suponiendo que posteriormente se conectara a una BD)
+
+        public long Id { get => _Id; set => _Id = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Apellido { get => _Apellido; set => _Apellido = value; }
+        public string NombreUsuario { get => _NombreUsuario; set => _NombreUsuario = value; }
+        public string Pasword { get => _Pasword; set => _Pasword = value; }
+        public string Mail { get => _Mail; set => _Mail = value; }
+
+
+
+        //Constructores
+        /*   public Usuarios(long id, string nombre, string apellido, string nombreUsuario, string pasword, string mail)
+               {
+                   _Id = id;
+                   _Nombre = nombre;
+                   _Apellido = apellido;
+                   _NombreUsuario = nombreUsuario;
+                   _Pasword = pasword;
+                   _Mail = mail;
+               }
+           */
+
+    }
+
+
+    #endregion
+
+    #region ClassProduct
+    public class Producto
+    {
+        //Atributos
+
+        private long _IdProducto;
+        private string _Descripcion;
+        private decimal _Costo;
+        private decimal _PrecioVenta;
+        private int _Stock;
+        private long _IdUsuario;
+
+        //Propiedades
+
+        public long IdProducto { get => _IdProducto; set => _IdProducto = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+        public decimal Costo { get => _Costo; set => _Costo = value; }
+        public decimal PrecioVenta { get => _PrecioVenta; set => _PrecioVenta = value; }
+        public int Stock { get => _Stock; set => _Stock = value; }
+        public long IdUsuario { get => _IdUsuario; set => _IdUsuario = value; }
+
+
+        //Constructor
+        /*  public Producto (long idProducto, string descripcion, decimal costo, decimal precioVenta, int stock, long idUsuario)
+          {
+              _IdProducto = idProducto;
+              _Descripcion = descripcion;
+              _Costo = costo;
+              _PrecioVenta = precioVenta;
+              _Stock = stock;
+              _IdUsuario = idUsuario;
+          }*/
+
+
+        #endregion
+    }
+
+    #region ClassProductSold
+    public class ProductoVendido
+    {
+        //Atributos
+
+        private long _Id;
+        private long _IdProductoVendido;
+        private int _Stock;
+        private long _IdVenta;
+
+        //Porpiedades
+        public long Id { get => _Id; set => _Id = value; }
+        public long IdProductoVendido { get => _IdProductoVendido; set => _IdProductoVendido = value; }
+        public int Stock { get => _Stock; set => _Stock = value; }
+        public long IdVenta { get => _IdVenta; set => _IdVenta = value; }
+
+
+        /*  //Constructor
+          public ProductoVendido(long id, long idProductoVendido, int stock, long idVenta)
+          {
+              _Id = id;
+              _IdProductoVendido = idProductoVendido;
+              _Stock = stock;
+              _IdVenta = idVenta;
+          }*/
+
+
+
+        #endregion
+    }
+
+    #region ClassSale
+    public class Venta
+    {
+        //Atributos
+
+        private long _Id;
+        private string _Comentarios;
+        //  private int _IdUsuario;
+
+
+        //Propiedades
+        public long Id { get => _Id; set => _Id = value; }
+        public string Comentarios { get => _Comentarios; set => _Comentarios = value; }
+        // public int IdUsuario { get => _IdUsuario; set => _IdUsuario = value; }
+
+
+        /*      //Constructor
+              public Venta(long id, string comentarios)//, int idUsuario)
+              {
+                  _Id = id;
+                  _Comentarios = comentarios;
+                //  _IdUsuario = idUsuario;
+              }*/
+        #endregion
+    }
+
+
+}
---
+++ Handler/ProductHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+
+namespace Entrega.Handler
+{
+    public static class ProductHandler
+    {
+        //Conector
+        internal static string _Cnn = "Data Source =.\\SQLEXPRESS;AttachDbFilename=C:\\BASE_DE_DATOS\\SistemaGestion_.mdf;Integrated Security = True; Connect Timeout = 30";
+
+        public static List<Producto> ObtenerProductos(string Descripcion)
+        {
+            List<Producto> ListProductos = new List<Producto>();
+
+            //Conexión a DB
+            using (SqlConnection conx = new SqlConnection(_Cnn))
+            {
+                //Comando a ejecutar
+                //using (SqlCommand comando = new SqlCommand("SELECT * FROM Producto WHERE Producto.IdUsuario = Id;", conx))
+                using (SqlCommand comando = new SqlCommand("SELECT * FROM Producto; ", conx))
+                {
+
+                    //Abrimos conexión
+                    conx.Open();
+                    //Creo DataReader y ejecuto para recuperar datos
+                    SqlDataReader reader = comando.ExecuteReader();
+
+                    if (reader.HasRows)//corrobora haya filas
+                    {
+                        while (reader.Read())//lee por fila
+                        {
+
+                            Producto productoLista = new Producto();
+
+                            productoLista.IdProducto = reader.GetInt64(0);
+                            productoLista.Descripcion = reader.GetString(1);
+                            productoLista.Costo = reader.GetDecimal(2);
+                            productoLista.PrecioVenta = reader.GetDecimal(3);
+                            productoLista.Stock = reader.GetInt32(4);
+                            productoLista.IdUsuario = reader.GetInt64(5);
+                            comando.Parameters.AddWithValue("IdUsuario", Descripcion);
+                            Console.WriteLine(@Descripcion);
+                            Console.WriteLine(productoLista);
+                            ListProductos.Add(productoLista);
+
+                            Console.WriteLine("ListaProductos", reader.HasRows);
+                        }
+
+                        Debug.WriteLine("aqui", ListProductos);
+
+                    }
+                    conx.Close();
+                    Debug.WriteLine("aqui2", ListProductos);
+                    return ListProductos;
+
+
+                }
+
+            }
+        }
+
+    }
+}
